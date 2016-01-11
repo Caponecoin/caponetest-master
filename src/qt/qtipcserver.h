a1146b0e049c24bd4083f3c5e2258f7f02b5e7f9
Caponecoin <caponecoin@gmail.com>
2016-01-11 21:58:43 +0100

    CaponeCoin
    X11 POW POS

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define CaponeCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "CaponeCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
